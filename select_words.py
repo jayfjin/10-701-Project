3b774ce428b5b08dfa88cf45aef44c75621124fc
Dan Ringwalt <dringw@gmail.com>
2015-12-02 12:53:15 -0500

    wordsel

--- select_words.py
+++ select_words.py
@@ -1,24 +1,23 @@
 import sys
-import numpy
+import numpy as np
 import gzip
-words = numpy.loadtxt('adblocktv/bow_selected.txt', dtype=bool)
-wordfeats = list(122 + numpy.where(words)[0])
+words = np.loadtxt('adblocktv/bow_selected.txt', dtype=bool)
+keepfeats = np.zeros(4126, bool)
+keepfeats[1:123] = True
+keepfeats[np.where(words)[0] + 122] = True
+keepfeats[4124:] = True
+newfeatmap = np.cumsum(keepfeats) - 1
 output = gzip.open(sys.argv[2], 'w')
 for line in gzip.open(sys.argv[1]):
     feats = line.strip().split()
     def filter_feat(feat):
         i, value = feat.split(':')
         i = int(i)
-        return i < 122 or i in wordfeats or i >= 4123
+        return keepfeats[i]
     def update_feat(feat):
         i, value = feat.split(':')
         i = int(i)
-        if i < 122:
-            return feat
-        elif i < 4123:
-            return str(122 + wordfeats.index(i)) + ':' + str(value)
-        else:
-            return str(i + len(wordfeats) - 4001) + ':' + str(value)
+        return str(newfeatmap[i]) + ':' + str(value)
     feats = feats[0:1] + map(update_feat, filter(filter_feat, feats[1:]))
     output.write(' '.join(feats) + '\n')
 output.flush()
